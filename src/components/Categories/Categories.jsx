d9c29e9356328ea9a6f9de35bb8e12cf13a22a62
Vinson 2 <vinsonhe2@gmail.com>
2025-07-19 10:32:44 -0700

    feat: replace shoes category with electronics

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 import styles from "./Categories.module.css";
 
-const Category = ({ children, category }) => {
+const Category = ({ children, category, endpoint }) => {
   return (
     <li className={styles[category]}>
-      <div className={styles.cardText}>{children}</div>
+      <Link to={`/shop/category/${endpoint}`} className={styles.categoryLink}>
+        <div className={styles.cardText}>{children}</div>
+      </Link>
     </li>
   );
 };
@@ -13,16 +16,16 @@ const Categories = () => {
     <section className={styles.categorySection}>
       <h2 className={styles.categorySectionHeading}>Shop By Category</h2>
       <ul className={styles.categoriesList}>
-        <Category category={"shoes"}>
-          <h3 className={styles.cardHeading}>Shoes</h3>
+        <Category category={"electronics"} endpoint={"electronics"}>
+          <h3 className={styles.cardHeading}>Electronics</h3>
         </Category>
-        <Category category={"jewelry"}>
+        <Category category={"jewelry"} endpoint={"jewelry"}>
           <h3 className={styles.cardHeading}>Jewelry</h3>
         </Category>
-        <Category category={"mensClothing"}>
+        <Category category={"mensClothing"} endpoint={"mens-clothing"}>
           <h3 className={styles.cardHeading}>Men's Clothing</h3>
         </Category>
-        <Category category={"womensClothing"}>
+        <Category category={"womensClothing"} endpoint={"womens-clothing"}>
           <h3 className={styles.cardHeading}>Women's Clothing</h3>
         </Category>
       </ul>
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Categories } from "../Categories/Categories.jsx";
 import styles from "./HomePage.module.css";
 
@@ -5,12 +6,14 @@ const HomePage = () => {
   return (
     <>
       <section className={styles.hero}>
-        <div className={styles.textContainer}>
-          <h1 className={styles.heading}>
-            This is <span className={styles.logo}>VInSION</span>
-          </h1>
-          <p className={styles.para}>Discover the Summer 2025 Collection.</p>
-        </div>
+        <Link to={"shop"} className={styles.heroLink}>
+          <div className={styles.textContainer}>
+            <h1 className={styles.heading}>
+              This is <span className={styles.logo}>VInSION</span>
+            </h1>
+            <p className={styles.para}>Discover the Summer 2025 Collection.</p>
+          </div>
+        </Link>
       </section>
       <Categories />
     </>
