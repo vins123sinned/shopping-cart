9969894366de3ce4304f3b40c8e36c090c9abc64
Vinson 2 <vinsonhe2@gmail.com>
2025-07-23 13:50:31 -0700

    feat: add responsive styling for cartItems

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,22 +7,28 @@ const CartItem = ({ title, price, category, imageLink, quantity, id }) => {
   return (
     <li>
       <article className={styles.cartItem}>
-        <div className={styles.leftCart}>
-          <img src={imageLink} alt="Product image" />
-        </div>
-        <div className={styles.rightCart}>
-          <h2 className={styles.itemTitle}>{title}</h2>
-          <Link
-            to={`/shop/category/${productLinkMap[category]}`}
-            className={styles.itemCategory}
-          >
-            {productHeadingMap[category]}
-          </Link>
-          <p className={styles.itemPrice}>${price}</p>
-          <div className={styles.priceQuantityContainer}>
-            <p className={styles.itemTotal}>${formatPrice(price * quantity)}</p>
-            <Quantity quantity={quantity} id={id} type={"cart"} />
+        <div className={styles.leftSection}>
+          <img
+            src={imageLink}
+            alt="Product image"
+            className={styles.itemImage}
+          />
+          <div className={styles.titleCategoryContainer}>
+            <h2 className={styles.itemTitle}>{title}</h2>
+            <Link
+              to={`/shop/category/${productLinkMap[category]}`}
+              className={styles.itemCategory}
+            >
+              {productHeadingMap[category]}
+            </Link>
           </div>
+        </div>
+
+        <p className={styles.itemPrice}>${formatPrice(price)}</p>
+
+        <div className={styles.totalQuantityContainer}>
+          <p className={styles.itemTotal}>${formatPrice(price * quantity)}</p>
+          <Quantity quantity={quantity} id={id} type={"cart"} />
         </div>
       </article>
     </li>
@@ -43,39 +49,38 @@ const Cart = () => {
   return (
     <section className={`${styles.cartSection} mainSection`}>
       <h1 className={styles.cartHeading}>Your Cart</h1>
+
       <div className={styles.flexDiv}>
-        <div className={styles.cartItemsContainer}>
-          <ul className={styles.cartItems}>
-            <li>
-              <p className={styles.itemCount}>{itemCount} items</p>
-            </li>
-            <li className={styles.cartItemsHeader}>
-              <p className={styles.productInformation}>Product</p>
-              <p className={styles.headerPara}>Price</p>
-              <p className={styles.headerPara}>Quantity</p>
-              <p className={styles.headerPara}>Total</p>
-            </li>
-            {cart.map((item) => (
-              <CartItem
-                title={item.title}
-                imageLink={item.imageLink}
-                price={item.price}
-                category={item.category}
-                quantity={item.quantity}
-                id={item.id}
-                key={item.id}
-              />
-            ))}
-          </ul>
-        </div>
-        <button
-          type="button"
-          className={styles.checkoutButton}
-          onClick={clearCart}
-        >
-          Checkout Now
-        </button>
+        <ul className={styles.cartItems}>
+          <li className={styles.itemCountList}>
+            <p>{itemCount} items</p>
+          </li>
+          <li className={styles.headerList}>
+            <p className={styles.headerProduct}>Product</p>
+            <p className={styles.headerPrice}>Price</p>
+            <p className={styles.headerQuantity}>Quantity</p>
+            <p className={styles.headerTotal}>Total</p>
+          </li>
+          {cart.map((item) => (
+            <CartItem
+              title={item.title}
+              imageLink={item.imageLink}
+              price={item.price}
+              category={item.category}
+              quantity={item.quantity}
+              id={item.id}
+              key={item.id}
+            />
+          ))}
+        </ul>
       </div>
+      <button
+        type="button"
+        className={styles.checkoutButton}
+        onClick={clearCart}
+      >
+        Checkout Now
+      </button>
     </section>
   );
 };
