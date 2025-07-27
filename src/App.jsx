3e9ac97ad7ffb287f99d85b855b6a994e035bc53
Vinson 2 <vinsonhe2@gmail.com>
2025-07-27 08:40:26 -0700

    feat: reset scroll position on route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { useEffect, useState } from "react";
-import { Outlet } from "react-router-dom";
+import { useEffect, useLayoutEffect, useState } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import { Navbar } from "./components/Navbar/Navbar.jsx";
 import { Footer } from "./components/Footer/Footer.jsx";
 import "./App.css";
 
-function App() {
+const ScrollToTop = () => {
+  const location = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return null;
+};
+
+const App = () => {
   const [cart, setCart] = useState(() => {
     // Lazy intializer for complex operations (runs only once on mount)
     const cartData = localStorage.getItem("cart");
@@ -17,11 +27,12 @@ function App() {
 
   return (
     <>
+      <ScrollToTop />
       <Navbar cart={cart} />
       <Outlet context={[cart, setCart]} />
       <Footer />
     </>
   );
-}
+};
 
 export default App;
